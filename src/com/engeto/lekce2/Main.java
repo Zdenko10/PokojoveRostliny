2f9deaf20c2ee9daeb1a28d796b683af1fa66a29
Zdenko10 <zdenkorybcak@gmail.com>
2022-03-16 14:07:50 +0100

    Commit všech souborů ze složky Pokojové rostliny

---
+++ src/com/engeto/lekce2/Main.java
@@ -0,0 +1,32 @@
+package com.engeto.lekce2;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    private static final String INPUT_FILE = "kvetiny.txt";
+
+    private static final String OUTPUT_FILE = "vystup.txt";
+
+    public static void main(String[] args) {
+        PlantList list = new PlantList();
+        try {
+            list.importPlantsFromFile(INPUT_FILE);
+        } catch (PlantException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.getPlant(i).getWateringInfo());
+        }
+        list.addPlant(new Plant("Amarylis v obýváku"));
+        list.addPlant(new Plant("Bazalka v kuchyni", 3, LocalDate.now()));
+        list.removePlant(2);
+
+        try {
+            list.exportToFile(OUTPUT_FILE);
+        } catch (PlantException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+    }
+}
